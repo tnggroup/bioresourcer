cdc65bd20b9b0b304706008a670406f0997b9ffc
laurameldrum <140835786+laurameldrum@users.noreply.github.com>
2025-05-29 15:52:04 +0100

    improved reminders script registration 1

--- manual_routines/registration_reminder_1.sql
+++ manual_routines/registration_reminder_1.sql
@@ -1,30 +1,42 @@
-/* GLAD reminder email (registration 1)
- * Date: 27/02/2024
- * Author: Mika Malouf
- */
+/*GLAD reminder email (registration 1) v2
+  Date: 22/05/2025
+  Author: Laura Meldrum */
  
+SELECT 
+jt.alias_matched AS Aliases,
+p.id AS 'Participant id', 
+p.first_name AS Forename,
+p.last_name AS Surname, 
+p.email AS 'Email address',
+p.phone AS 'Phone number',
+p.registered_at, 
+cfr.created_at AS consented
 
-USE mhbior;
- 
-SELECT
- JSON_UNQUOTE(JSON_EXTRACT(ps.aliases, '$[0]')) aliases,
- ps.id as 'Participant id',
- ps2.consented_at,
- ps.first_name as forename,
- last_name as surname, 
- ps.email,
- ps.phone as mobile,
- ps2.export_eligibility_state,
- ps.registered_at
+FROM participants p
+CROSS JOIN JSON_TABLE(p.aliases, '$[*]' COLUMNS(alias_matched VARCHAR(255) PATH '$')) AS jt
+JOIN participant_study ps ON ps.id = p.id
+LEFT JOIN consent_form_responses cfr ON cfr.participant_id = p.id 
+LEFT JOIN consent_forms cf ON cf.id = cfr.consent_form_id AND cf.study_id = ps.study_id
+LEFT JOIN communications c ON c.participant_id = p.id AND c.study_id = ps.study_id
+LEFT JOIN withdrawals w on w.participant_id = p.id AND w.study_id = ps.study_id
 
-FROM participants ps, samples s, participant_study ps2, studies ss
-WHERE ps.id = s.participant_id 
-AND ps2.participant_id = ps.id
-AND ss.id = ps2.study_id
-AND account_type = 'Active'
-AND ps.withdrew_at IS NULL
-AND ss.name = 'GLAD'
-AND ps.email IS NOT NULL
-AND date_kit_sent IS NULL
-AND ps2.consented_at IS NULL
-AND ps.registered_at IS NOT NULL;
+WHERE p.account_type = 'Active'
+AND jt.alias_matched REGEXP '^GLAD[0-9]{6}$'
+AND (p.first_name NOT LIKE 'test%' OR p.last_name NOT LIKE 'test%')
+AND cfr.created_at IS NULL #has NOT consented
+AND c.sent_at IS NULL #has NOT already received a reminder (check this)
+AND w.withdrew_at IS NULL #has NOT withdrawn (check this)
+AND ps.study_id = 1 #GLAD
+
+ORDER BY Aliases
+;
+
+/*
+Issues - LM 22/05/2025
+are participants who have already consented still in this report?
+are all withdrawn participants removed from this report?
+does this remove all people who have already had a registration reminder? no it does not (obscured-1699250890-654882ca8c720@example.co.uk)
+EDGI participants are in this list
+why are there some consent dates in the consented_at column that do not match MHBIOR?
+
+Testing: go through the usual registration reminders process and check if there are participants who are being removed in the merging steps*/
