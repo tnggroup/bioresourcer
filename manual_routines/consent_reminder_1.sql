4e183e849fcfb41417ba5075ad863b3fe6339b26
laurameldrum <140835786+laurameldrum@users.noreply.github.com>
2025-06-05 15:12:17 +0100

    added new version of the script

--- manual_routines/consent_reminder_1.sql
+++ manual_routines/consent_reminder_1.sql
@@ -1,27 +1,40 @@
-/* GLAD consented participants who have not yet completed their baseline questionnaire- 
- * Date: 18/06/2024
- * Author: Chelsea Mika Malouf
- */
+/*GLAD consent reminder email 1 (GLAD consented not finished survey v2)
+  Date: 22/05/2025
+  Author: Laura Meldrum */
  
- USE mhbior;
- 
- SELECT
- JSON_UNQUOTE(JSON_EXTRACT(p.aliases, '$[0]')) aliases,
- p.id as 'Participant id',
- p.email,
- p.phone as mobile,
- first_name forename,
- last_name surname,
- ps.export_eligibility_state,
- consented_at,
- ps.manual_eligibility_state
- 
- FROM participants p, participant_study ps, studies s
- WHERE p.id = ps.participant_id 
- AND s.id = ps.study_id 
- AND ps.export_eligibility_state IS NULL 
- AND ps.manual_eligibility_state IS NULL
- AND ps.consented_at IS NOT NULL
- AND p.withdrew_at IS NULL
- AND s.name = 'GLAD'
- AND account_type = 'Active';
+SELECT 
+jt.alias_matched AS Aliases,
+p.id AS 'Participant id', 
+p.first_name AS forename,
+p.last_name AS surname, 
+p.email AS 'email address',
+p.phone AS 'phone number',
+cfr.created_at AS consented,
+ps.manual_eligibility_state,
+ps.export_eligibility_state
+#c.type, 
+#c.number, 
+#c.sent_at
+
+FROM participants p
+CROSS JOIN JSON_TABLE(p.aliases, '$[*]' COLUMNS(alias_matched VARCHAR(255) PATH '$')) AS jt
+JOIN participant_study ps ON ps.id = p.id
+LEFT JOIN consent_form_responses cfr ON cfr.participant_id = p.id 
+LEFT JOIN consent_forms cf ON cf.id = cfr.consent_form_id AND cf.study_id = ps.study_id
+LEFT JOIN communications c ON c.participant_id = p.id AND c.study_id = ps.study_id
+LEFT JOIN withdrawals w on w.participant_id = p.id AND w.study_id = ps.study_id
+
+WHERE p.account_type = 'Active'
+AND ps.study_id = 1 #GLAD
+AND jt.alias_matched REGEXP '^GLAD[0-9]{6}$'
+AND (p.first_name NOT LIKE 'test%' OR p.last_name NOT LIKE 'test%')
+AND cfr.created_at IS NOT NULL #has consented
+AND COALESCE(ps.manual_eligibility_state, 0) = 0
+AND COALESCE(ps.export_eligibility_state, 0) = 0 #is not eligible (this needs to be looked at)
+AND c.type != 'Consent Reminder' #has NOT already received a reminder (this needs to be edited to be has not received the first consent reminder)
+AND w.withdrew_at IS NULL #has NOT withdrawn
+
+ORDER BY Aliases
+;
+
+#04/06/2025 LM. Issues: eligibility state not correct for participants, for next reminders, how do we select only the ppts who have been sent reminder 1 but not the others 
